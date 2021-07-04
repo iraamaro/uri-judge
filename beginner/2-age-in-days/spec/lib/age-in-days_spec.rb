7e1e2c42648528d6245372e0d086579c4a17c212
iramaro <iramaro@disroot.org>
2021-07-04 18:57:52 -0300

    URI 1020 finished.

--- beginner/2-age-in-days/spec/lib/age-in-days_spec.rb
+++ beginner/2-age-in-days/spec/lib/age-in-days_spec.rb
@@ -7,7 +7,11 @@ describe AgeInDays do
     expect(days.split_days(400)).to eq('1 anos(s) 1 mes(es) 5 dia(s)')
   end
   it 'when pass 800 days' do
+    days = AgeInDays.new(800)
+    expect(days.split_days(800)).to eq('2 anos(s) 2 mes(es) 10 dia(s)')
   end
   it 'when pass 30 days' do
+    days = AgeInDays.new(30)
+    expect(days.split_days(30)).to eq('0 anos(s) 1 mes(es) 0 dia(s)')
   end
 end
